c5f52b99e1322c48f4f0a86c78adbcfda66b100f
EC2 Default User <ec2-user@ip-10-16-10-57.us-west-2.compute.internal>
2024-06-05 13:26:32 +0000

    added files

--- analyze-insulin.py
+++ analyze-insulin.py
@@ -1,12 +1,2 @@
-filename = '/preproinsulin-seq.txt'
-# Function to read the contents of the file
-def read_file(filename):
-    with open(filename, 'r') as file:
-        contents = file.read()
-    return contents
-
-# Read the file
-file_contents = read_file(filename)
-
-# Print the contents of the file
-print(file_contents)
+f = open("lab10-12/preproinsulin-seq-clean.txt", "r")
+print(f.read(24))
